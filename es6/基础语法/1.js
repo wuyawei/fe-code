6f9f8fb9e0263a6322f469f652baf6d0d39db354
wuyw <wuyw@zonekey.com.cn>
2019-02-19 16:57:11 +0800

    数组的扩展

--- es6/基础语法/1.js
+++ es6/基础语法/1.js
@@ -1,9 +1,39 @@
-let p = [1,2,3];
+// 扩展运算符、rest运算符
+/*let p = [1,2,3];
 function test(args) {
     [...args].forEach(v => console.log('test', v));
 }
 function test1(...args) {
     args.forEach(v => console.log('test1', v));
-}
-test(p);
-test1(...p);
+}*/
+// test(p);
+// test1(...p);
+// 1  2  3
+
+/*let a1 = [1, 2];
+let a2 = [...a1];
+let [...a3] = a1;*/
+// console.log(a2, a3);
+// [1, 2]
+
+/*let s = [...'hello'];
+console.log(s);*/
+
+/*let dom = document.querySelector('p');
+console.log([...dom]);*/
+
+// 数组
+// let arrayLike = {
+//     '0': 1,
+//     '1': 2,
+//     '2': 3,
+//     length: 3
+// };
+// let arr2 = Array.from(arrayLike);
+// let arr2 = Array.from(arrayLike, v => 2 * v);
+// let arr2 = Array.from(arrayLike).map(v => 2 * v);
+// console.log(arr2);
+
+// let p = {price: 3};
+// let a = [2, 3, 5, 1].find(function(n) { return n > this.price }, p);
+// console.log(a);
