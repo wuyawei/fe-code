c50d91ee7340e309c7c65405cb1d0c97438c05d3
wuyw <w2499115366@126.com>
2019-02-22 08:53:31 +0800

    对象的扩展

--- es6/基础语法/1.js
+++ es6/基础语法/1.js
@@ -1,28 +1,28 @@
 // 扩展运算符、rest运算符
-/*let p = [1,2,3];
-function test(args) {
-    [...args].forEach(v => console.log('test', v));
-}
-function test1(...args) {
-    args.forEach(v => console.log('test1', v));
-}*/
+// let p = [1,2,3];
+// function test(args) {
+//     [...args].forEach(v => console.log('test', v));
+// }
+// function test1(...args) {
+//     args.forEach(v => console.log('test1', v));
+// }
 // test(p);
 // test1(...p);
 // 1  2  3
 
-/*let a1 = [1, 2];
-let a2 = [...a1];
-let [...a3] = a1;*/
+// let a1 = [1, 2];
+// let a2 = [...a1];
+// let [...a3] = a1;
 // console.log(a2, a3);
 // [1, 2]
 
-/*let s = [...'hello'];
-console.log(s);*/
+// let s = [...'hello'];
+// console.log(s);
 
-/*let dom = document.querySelector('p');
-console.log([...dom]);*/
+// let dom = document.querySelector('p');
+// console.log([...dom]);
 
-// 数组
+// es6数组方法
 // let arrayLike = {
 //     '0': 1,
 //     '1': 2,
@@ -42,6 +42,36 @@ console.log([...dom]);*/
 // let a = i++;
 // let b = ++i;
 // console.log(a, b); // 0 2
+
+// es5数组方法
+// let arr = [{name: 'a', id: '1'}, {name: 'a', id: '2'}];
+// let f1 = arr.every(v => {
+//    return v.id === '1';
+// });
+// let f2 = arr.some(v => {
+//     return v.id === '1';
+// });
+// let f3 = arr.filter(v => {
+//     return v.id === '1';
+// });
+// let f4 = arr.map(v => {
+//     return v.id;
+// });
+// console.log(f1,f2,f3,f4);
+// let arr = [{name: '红烧牛肉饭', price: 29}, {name: '黄焖鸡米饭', price: 22}, {name: '农家小炒肉', price: 28}];
+// let price_all = arr.reduce((prev, curr, index, arr) => {
+//     return prev + curr.price;
+// }, arr[0].price);
+// console.log(price_all);
+// let goods = arr.reduce((prev, curr, index, arr) => {
+//     if (index === arr.length -1) {
+//         return prev + curr.name;
+//     }
+//     return prev + curr.name + ' + ';
+// }, '您选择的商品为：');
+// console.log(goods);
+
+// 迭代器
 // let obj = { // 给对象添加迭代器
 //     [Symbol.iterator]() {
 //         return {
@@ -57,32 +87,25 @@ console.log([...dom]);*/
 // for (let v of obj) {
 //     console.log(v)
 // }
-// let arr = [{name: 'a', id: '1'}, {name: 'a', id: '2'}];
-// let f1 = arr.every(v => {
-//    return v.id === '1';
-// });
-// let f2 = arr.some(v => {
-//     return v.id === '1';
-// });
-// let f3 = arr.filter(v => {
-//     return v.id === '1';
-// });
-// let f4 = arr.map(v => {
-//     return v.id;
-// });
-// console.log(f1,f2,f3,f4);
 
-let arr = [{name: '红烧牛肉饭', price: 29}, {name: '黄焖鸡米饭', price: 22}, {name: '农家小炒肉', price: 28}];
-let price_all = arr.reduce((prev, curr, index, arr) => {
-    return prev + curr.price;
-}, arr[0].price);
-console.log(price_all);
+// 对象的扩展
+let obj = { // es5不能在这里写[]
+    name: 'lisi',
+    age: 18
+};
+obj.name = 'lisisi';
+obj['age'] = 18;
+// [] 里可以写表达式
+let say = 'say';
+obj[say] = function(){
+    console.log(this.name);
+};
+obj.say();
 
-let goods = arr.reduce((prev, curr, index, arr) => {
-    if (index === arr.length -1) {
-        return prev + curr.name;
+let wangwu = {
+    name: 'wangwu',
+    [say]() {
+        console.log(this.name);
     }
-    return prev + curr.name + ' + ';
-}, '您选择的商品为：');
-
-console.log(goods);
+};
+wangwu.say();
