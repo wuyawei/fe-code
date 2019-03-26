6ce68df4cf5d2ef633517a83b42bd92ea00fa3e8
wuyw <wuyw@zonekey.com.cn>
2019-03-26 18:31:04 +0800

    js正则

--- JavaScript/js基础/正则/1.js
+++ JavaScript/js基础/正则/1.js
@@ -2,6 +2,10 @@
 // console.log('12225'.match(reg));
 
 
-let reg = /12[34]5/g;
-console.log('12225'.match(reg)); // null
-console.log('1235'.match(reg)); // [ '1235' ]
+// let reg = /12[34]5/g;
+// console.log('12225'.match(reg)); // null
+// console.log('1235'.match(reg)); // [ '1235' ]
+
+let reg = /2{0,}/g;
+// console.log('62225'.match(reg));
+console.log('1'.match(reg));
