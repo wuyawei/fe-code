81a5af1d053b5601c9a9f06adc7e1f075de75db4
wuyw <w2499115366@126.com>
2019-04-06 23:07:36 +0800

    数据劫持

--- JavaScript/defineProperty/3.js
+++ JavaScript/defineProperty/3.js
@@ -1,12 +1,22 @@
-function aa(val) {
-    console.log(val, arguments)
-    val = 1;
-    console.log(val, arguments)
-    bb(val);
+function defineReactive(obj, key, val) {
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            console.log('get');
+            return val;
+        },
+        set: function(newVal) {
+            console.log('set');
+            val += newVal;
+        }
+    });
 }
-function bb(v) {
-    console.log(v)
-}
-let a = 2;
-aa(a);
-console.log(a);
+let obj = {name: '成龙大哥', say: '：其实我之前是拒绝拍这个游戏广告的，'};
+Object.keys(obj).forEach(k => {
+    defineReactive(obj, k, obj[k]);
+});
+console.log(obj.name + obj.say);
+
+// obj.say = '后来我试玩了一下，哇，好热血，蛮好玩的';
+// console.log(obj.name + obj.say);
