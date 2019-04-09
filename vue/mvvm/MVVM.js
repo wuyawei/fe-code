16428b5d62aead8c6a39783d9621d0c0f638f882
wuyw <wuyw@zonekey.com.cn>
2019-04-09 17:29:29 +0800

    验证

--- vue/mvvm/MVVM.js
+++ vue/mvvm/MVVM.js
@@ -1,2 +1,35 @@
-class Dep{
+class Dep {
+    constructor(){
+        this.subs = []; // watcher 观察者集合
+    }
+    // 订阅
+    addSub(watcher){ // 添加 watcher
+        this.subs.push(watcher);
+    }
+    // 发布
+    notify(){
+        this.subs.forEach(w => w.update());
+    }
+}
+
+class Watcher{
+    constructor(vm,expr,cb){
+        this.vm = vm;
+        this.expr = expr;
+        this.cb = cb;
+        this.value = this.get(); // 保存旧值
+    }
+    get(){
+        Dep.target = this;
+        let value = util.getValue(this.vm, this.expr);
+        Dep.target = null;
+        return value;
+    }
+    update(){
+        let newValue = util.getValue(this.vm,this.expr);
+        if(newValue !== this.value){
+            this.cb(newValue);
+            this.value = newValue;
+        }
+    }
 }
