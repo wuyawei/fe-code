e30bafbccd0571eb0934f52905d0cf4c4135a602
wuyw <wuyw@zonekey.com.cn>
2019-04-10 10:02:39 +0800

    推荐阅读

--- JavaScript/defineProperty/1.js
+++ JavaScript/defineProperty/1.js
@@ -1,16 +1,16 @@
-let VchatInfo = {};
-let Input = document.getElementById('input');
-let introduceDom = document.getElementById('introduce');
-Object.defineProperty(VchatInfo, "introduce", {
-    get: function(){
-        return introduceDom.innerHTML;
-    },
-    set: function(introduce){
-        introduceDom.innerHTML = introduce;
-    }
-});
-VchatInfo.introduce = Input.value = "vchat是一个社交聊天系统";
-Input.oninput = function() {
-    VchatInfo.introduce = Input.value;
-    console.log(VchatInfo.introduce);
-};
+// let VchatInfo = {};
+// let Input = document.getElementById('input');
+// let introduceDom = document.getElementById('introduce');
+// Object.defineProperty(VchatInfo, "introduce", {
+//     get: function(){
+//         return introduceDom.innerHTML;
+//     },
+//     set: function(introduce){
+//         introduceDom.innerHTML = introduce;
+//     }
+// });
+// VchatInfo.introduce = Input.value = "vchat是一个社交聊天系统";
+// Input.oninput = function() {
+//     VchatInfo.introduce = Input.value;
+//     console.log(VchatInfo.introduce);
+// };
