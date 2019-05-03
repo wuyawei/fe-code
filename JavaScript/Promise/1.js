de1fe6bef96aa075c0b22b7d2caa1b44bc6e384c
wuyw <w2499115366@126.com>
2019-05-03 15:44:00 +0800

    promise

---
+++ JavaScript/Promise/1.js
@@ -0,0 +1,137 @@
+/**
+ * Created by wyw on 2019/5/3.
+ */
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+function Promise(executor) {
+    let self = this;
+    self.status = PENDING;
+    self.onFulfilled = [];//成功的回调
+    self.onRejected = []; //失败的回调
+    //PromiseA+ 2.1
+    function resolve(value) {
+        if (self.status === PENDING) {
+            self.status = FULFILLED;
+            self.value = value;
+            console.log(self.onFulfilled)
+            self.onFulfilled.forEach(fn => fn());//PromiseA+ 2.2.6.1
+        }
+    }
+
+    function reject(reason) {
+        if (self.status === PENDING) {
+            self.status = REJECTED;
+            self.reason = reason;
+            self.onRejected.forEach(fn => fn());//PromiseA+ 2.2.6.2
+        }
+    }
+
+    try {
+        executor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    //PromiseA+ 2.2.1 / PromiseA+ 2.2.5 / PromiseA+ 2.2.7.3 / PromiseA+ 2.2.7.4
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason };
+    let self = this;
+    //PromiseA+ 2.2.7
+    let promise2 = new Promise((resolve, reject) => {
+        if (self.status === FULFILLED) {
+            //PromiseA+ 2.2.2
+            //PromiseA+ 2.2.4 --- setTimeout
+            setTimeout(() => {
+                try {
+                    //PromiseA+ 2.2.7.1
+                    let x = onFulfilled(self.value);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    //PromiseA+ 2.2.7.2
+                    reject(e);
+                }
+            });
+        } else if (self.status === REJECTED) {
+            //PromiseA+ 2.2.3
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.reason);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        } else if (self.status === PENDING) {
+            self.onFulfilled.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(self.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+            self.onRejected.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(self.reason);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+        }
+    });
+    return promise2;
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+    let self = this;
+    //PromiseA+ 2.3.1
+    if (promise2 === x) {
+        reject(new TypeError('Chaining cycle'));
+    }
+    if (x && typeof x === 'object' || typeof x === 'function') {
+        let used; //PromiseA+2.3.3.3.3 只能调用一次
+        try {
+            let then = x.then;
+            if (typeof then === 'function') {
+                //PromiseA+2.3.3
+                then.call(x, (y) => {
+                    //PromiseA+2.3.3.1
+                    if (used) return;
+                    used = true;
+                    resolvePromise(promise2, y, resolve, reject);
+                }, (r) => {
+                    //PromiseA+2.3.3.2
+                    if (used) return;
+                    used = true;
+                    reject(r);
+                });
+
+            }else{
+                //PromiseA+2.3.3.4
+                if (used) return;
+                used = true;
+                resolve(x);
+            }
+        } catch (e) {
+            //PromiseA+ 2.3.3.2
+            if (used) return;
+            used = true;
+            reject(e);
+        }
+    } else {
+        //PromiseA+ 2.3.3.4
+        resolve(x);
+    }
+}
+
+let p = new Promise((resolve, reject) => {
+    resolve(1)
+}).then(r => console.log(r));
--- JavaScript/Promise/promise7.js
+++ JavaScript/Promise/promise7.js
@@ -1,96 +1,104 @@
 /**
  * Created by wyw on 2018/12/17.
  */
-// function Promise(Fn){
-//     this.value;
-//     this.reason;
-//     this.resolves = [];
-//     this.rejects = [];
-//     this.status = 'PENDING';
-//
-//     if(this.status === 'PENDING') {
-//         let transition = (status, val) => {
-//             setTimeout(_ => {
-//                 this.status = status;
-//                 let f = status === 'FULFILLED',
-//                     queue = this[f ? 'resolves' : 'rejects'];
-//                 queue.forEach(fn => val = fn(val) || val);
-//                 this[f ? 'value' : 'reason'] = val;
-//             });
-//         };
-//
-//         function resolve(value) {
-//             transition('FULFILLED', value);
-//         }
-//
-//         function reject(reason) {
-//             transition('REJECTED', reason);
-//         }
-//     }
-//     try {
-//         Fn(resolve, reject);
-//     }
-//     catch(err) {
-//         reject(err);
-//     }
-// }
-//
-// Promise.prototype.then = function(onFulfilled, onRejected) {
-//     let promise = this;
-//     return new Promise((resolve, reject) => {
-//         function success(value) {
-//             let val = typeof onFulfilled === 'function' && onFulfilled(value) || value;
-//             if(val && typeof val['then'] === 'function'){ // 判断是否有then方法
-//                 val.then(function(value){ // 如果返回的是Promise 则直接执行得到结果后再调用后面的then方法
-//                     resolve(value);
-//                 },function(reason){
-//                     reject(reason);
-//                 });
-//             }else{
-//                 resolve(val);
-//             }
-//         }
-//         function erro (reason) {
-//             let rea = typeof onRejected === 'function' && onRejected(reason) || reason;
-//             reject(rea);
-//         }
-//         if (promise.status === 'PENDING') {
-//             promise.resolves.push(success);
-//             promise.rejects.push(erro);
-//         } else if (promise.status === 'FULFILLED') {
-//             success(promise.value);
-//         } else if (promise.status === 'REJECTED') {
-//             erro(promise.reason);
-//         }
-//     });
-// };
-//
-//
-// Promise.prototype.catch = function(onRejected){
-//     return this.then(null, onRejected);
-// };
+function Promise(Fn){
+    this.value;
+    this.reason;
+    this.resolves = [];
+    this.rejects = [];
+    this.status = 'PENDING';
 
-function getInfo(success, fail) {
-    return new Promise((resolve, reject) => {
-        setTimeout(_ => {
-            let ran = Math.random();
-            console.log(success, ran);
-            if (ran > 0.5) {
-                resolve(success);
-            } else {
-                reject(fail);
-            }
-        }, 200);
-    })
+    if(this.status === 'PENDING') {
+        let transition = (status, val) => {
+            setTimeout(_ => {
+                this.status = status;
+                let f = status === 'FULFILLED',
+                    queue = this[f ? 'resolves' : 'rejects'];
+                queue.forEach(fn => val = fn(val) || val);
+                this[f ? 'value' : 'reason'] = val;
+            });
+        };
+
+        var resolve = (value) => {
+            transition('FULFILLED', value);
+        };
+
+        var reject = (reason) => {
+            transition('REJECTED', reason);
+        }
+    }
+    try {
+        Fn(resolve, reject);
+    }
+    catch(err) {
+        reject(err);
+    }
 }
 
-getInfo('Vchat', 'fail').then(res => {
-    console.log('1', res);
-    return getInfo('可以线上预览了', 'erro');
-}).catch(err => {
-    console.log('2', err);
-}).then(res => {
-    console.log('3', res);
-}).catch(err => {
-    console.log('4', err);
-});
+Promise.prototype.then = function(onFulfilled, onRejected) {
+    let promise = this;
+    return new Promise((resolve, reject) => {
+        function success(value) {
+            let val = typeof onFulfilled === 'function' && onFulfilled(value) || value;
+            if(val && typeof val['then'] === 'function'){ // 判断是否有then方法
+                val.then(function(value){ // 如果返回的是Promise 则直接执行得到结果后再调用后面的then方法
+                    resolve(value);
+                },function(reason){
+                    reject(reason);
+                });
+            }else{
+                resolve(val);
+            }
+        }
+        function erro (reason) {
+            let rea = typeof onRejected === 'function' && onRejected(reason) || reason;
+            reject(rea);
+        }
+        if (promise.status === 'PENDING') {
+            promise.resolves.push(success);
+            promise.rejects.push(erro);
+        } else if (promise.status === 'FULFILLED') {
+            success(promise.value);
+        } else if (promise.status === 'REJECTED') {
+            erro(promise.reason);
+        }
+    });
+};
+
+
+Promise.prototype.catch = function(onRejected){
+    return this.then(null, onRejected);
+};
+
+// function getInfo(success, fail) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(_ => {
+//             let ran = Math.random();
+//             console.log(success, ran);
+//             if (ran > 0.5) {
+//                 resolve(success);
+//             } else {
+//                 reject(fail);
+//             }
+//         }, 200);
+//     })
+// }
+//
+// getInfo('Vchat', 'fail').then(res => {
+//     console.log('1', res);
+//     return getInfo('可以线上预览了', 'erro');
+// }).catch(err => {
+//     console.log('2', err);
+// }).then(res => {
+//     console.log('3', res);
+// }).catch(err => {
+//     console.log('4', err);
+// });
+
+const p2 = new Promise((resolve, reject) => {
+    throw new Error('报错了');
+})
+    .then(result => {console.log(result)})
+    .catch(e => {
+        console.log(e);
+    });
