f86a080f079c9e5f67c348da1ec8ce8449936756
wuyw <w2499115366@126.com>
2019-06-13 08:39:13 +0800

    hook 井字棋

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square(props) {
+    return (
+       <div className="square">{props.value}</div>
+    );
+}
+function Board() {
+    const a = new Array(9).fill('');
+    return (
+        <div className="Board">
+            {
+                a.map((v, i) => <Square key={i} value={i}/>)
+            }
+        </div>
+    )
+}
+function Game() {
+    const m = '下一个：X';
+    return (
+        <div className="Game">
+            <div className="game-board">
+                <Board/>
+            </div>
+            <div className="game-info">
+                <div className="status">
+                    {m}
+                </div>
+                <ol></ol>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default Game;
