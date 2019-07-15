683111990b6ed04379c0b74e988e3b596e41280b
wuyawei <wuyawei@koolearn.com>
2019-07-15 20:00:43 +0800

    _curry

--- JavaScript/函数式编程学习/index.js
+++ JavaScript/函数式编程学习/index.js
@@ -2,17 +2,18 @@ const {log} = console;
 function add(a,b,c) {
     return a + b + c;
 }
-
 function curry(fn) {
     const len = fn.length;
     return function bindfn() {
         if(arguments.length < len) {
-            return bindfn.bind(null, ...arguments);
+            return bindfn.bind(null, ...arguments); // 关键：保存参数，并在调用时和后面的参数一起传入 bindfn
         } else {
             return fn.call(null, ...arguments);
         }
     }
 }
+
+// const _curry = fn => (...args) => fn.bind(null, ...args);
 
 const curryadd = curry(add);
 log(curryadd(1)(2)(3));
