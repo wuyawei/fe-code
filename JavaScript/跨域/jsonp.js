81df9985dddf9d3a7b93d3996185f51d24458dc2
wuyawei <wuyawei@koolearn.com>
2019-10-21 10:51:53 +0800

    IIFE

--- JavaScript/跨域/jsonp.js
+++ JavaScript/跨域/jsonp.js
@@ -1,18 +1,18 @@
 /**
  * Created by wyw on 2019/1/26.
  */
-const jsonp = function ({url, params, callback}) {
-    let script = document.createElement('script');
+const jsonp = function ({ url, params, callback }) {
+    const script = document.createElement('script');
     return new Promise((resolve, reject) => {
         window[callback] = function (data) {
             resolve(data);
             document.body.removeChild(script);
         };
-        params = {...params, callback};
-        let arrs = [];
-        for (let key in params) {
-            arrs.push(`${key}=${params[key]}`);
-        }
+        params = { ...params, callback };
+        const arrs = [];
+        Object.keys(params).forEach((k) => {
+            arrs.push(`${k}=${params[k]}`);
+        });
         script.src = `${url}?${arrs.join('&')}`;
         document.body.appendChild(script);
     });
--- react/hooks.js
+++ react/hooks.js
@@ -1,11 +1,77 @@
-(function() {
-    var _state;
-    function useState(initialValue) {
-      _state = _state | initialValue;
-      function setState(newState) {
-        _state = newState;
-        render();
+const hooks = (function() {
+  const HOOKS = [];
+  let currentIndex = 0;
+  const Tick = {
+      render: null,
+      queue: [],
+      push: function(task) {
+          this.queue.push(task);
+      },
+      nextTick: function() {
+          setTimeout(() => {
+              // console.log(this.queue);
+              if (this.queue.length) { // 一次渲染后，全部出栈，确保单次事件循环不会重复渲染
+                  this.queue.forEach(f => f()); // 依次执行队列中所有任务
+                  currentIndex = 0; // 重置计数
+                  this.queue = []; // 清空队列
+                  this.render && this.render();
+              }
+          })
       }
-      return [_state, setState];
-    }
+  };
+  function useState(initialState) {
+      HOOKS[currentIndex] = HOOKS[currentIndex] || (typeof initialState === 'function' ? initialState() : initialState);
+      const memoryCurrentIndex = currentIndex; // currentIndex 是全局可变的，需要保存本次的
+      const setState = p => {
+          let newState = p;
+          if (typeof p === 'function') {
+              newState = p(HOOKS[memoryCurrentIndex]);
+          }
+          if (newState === HOOKS[memoryCurrentIndex]) return;
+          const update = () => {
+              HOOKS[memoryCurrentIndex] = newState;
+          }
+          Tick.push(update);
+          Tick.nextTick();
+      };
+      return [HOOKS[currentIndex++], setState];
+  }
+  function useEffect(fn, deps) {
+      const effect = HOOKS[currentIndex];
+      const _deps = effect && effect._deps;
+      const hasChange = _deps ? !deps.every((v, i) => _deps[i] === v) : true;
+      const memoryCurrentIndex = currentIndex; // currentIndex 是全局可变的
+      if (!deps || hasChange) {
+          const _effect = effect && effect._effect;
+          setTimeout(() => {
+              typeof _effect === 'function' && _effect(); // 每次先判断一下有没有上一次的副作用需要卸载
+              const ef = fn();
+              HOOKS[memoryCurrentIndex] = {...HOOKS[memoryCurrentIndex], _effect: ef}; // 更新effects
+          })
+      }
+      HOOKS[currentIndex++] = {_deps: deps, _effect: null};
+  }
+  function useReducer(reducer, initialState) {
+      const [state, setState] = useState(initialState);
+      const update = (state, action) => {
+          const result = reducer(state, action);
+          setState(result);
+      }
+      const dispatch = update.bind(null, state);
+      return [state, dispatch];
+  }
+  function useMemo(fn, deps) {
+      const hook = HOOKS[currentIndex];
+      const _deps = hook && hook._deps;
+      const hasChange = _deps ? !deps.every((v, i) => _deps[i] === v) : true;
+      const memo = hasChange ? fn() : hook.memo;
+      HOOKS[currentIndex++] = {_deps: deps, memo};
+      return memo;
+  }
+  function useCallback(fn, deps) {
+      return useMemo(() => fn, deps);
+  }
+  return {
+    Tick, useState, useEffect, useReducer, useMemo, useCallback
+  }
 })();
