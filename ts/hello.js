4a450af631ef398485b65543b75f7fea5e511821
wuyawei <wuyawei@koolearn.com>
2019-10-28 19:00:39 +0800

    feat:ts

---
+++ ts/hello.js
@@ -0,0 +1,6 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+debugger;
+var user = 'Tom';
+console.log(sayHello(user));
---
+++ ts/hello.ts
@@ -0,0 +1,7 @@
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+let user = '2';
+console.log(sayHello(user));
+
+let show: boolean = true;
