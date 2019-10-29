d97588a11937258a48f74d359ba0652828524788
wuyawei <wuyawei@koolearn.com>
2019-10-29 18:55:54 +0800

    ts-update

--- ts/hello.js
+++ ts/hello.js
@@ -1,6 +1,17 @@
 function sayHello(person) {
     return 'Hello, ' + person;
 }
-debugger;
-var user = 'Tom';
+var user = '2';
 console.log(sayHello(user));
+var isMan = true;
+var age = 12;
+var Name = '张三';
+var u = undefined;
+var n = null;
+function logInfo() {
+    console.log("" + Name + (isMan ? '是' : '不是') + "\u7537\u751F\uFF0C\u4ECA\u5E74" + age + "\u5C81;");
+    return n;
+}
+logInfo();
+var value = 12;
+console.log(value.text);
--- ts/hello.ts
+++ ts/hello.ts
@@ -4,4 +4,44 @@ function sayHello(person: string) {
 let user = '2';
 console.log(sayHello(user));
 
-let show: boolean = true;
+let isMan: boolean = true;
+let age: number = 12;
+let Name: string = '张三';
+let u: undefined = undefined;
+let n: null = null;
+
+function logInfo():void {
+    console.log(`${Name}${isMan ? '是' : '不是'}男生，今年${age}岁;`);
+    return n;
+}
+logInfo()
+let value: any = 12;
+console.log(value.text)
+let strAndNum: string | number = 12;
+strAndNum = '13';
+console.log(strAndNum)
+
+interface Person {
+    readonly id ?: number; // 只读且可以不写
+    name: string;
+    age?: number;
+    [propName: string]: any;
+}
+
+let tom: Person = {
+    name: 'Tom',
+    gender: 'male'
+};
+
+setTimeout(() => {
+    tom = {
+        id: 123,
+        name: 'Tom',
+        gender: 'male'
+    }
+    console.log(tom)
+}, 1000)
+
+let numList: number[] = [1, 3, 5, 7, 9];
+let strList: Array<string> = ['1', '3', '5', '7', '9'];
+let anyList: any[] = ['1', 3, {}];
