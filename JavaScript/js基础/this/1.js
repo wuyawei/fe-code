8e9f4e1030fe0d1ec2662e113ba06afcfe4cee87
wuyawei <wuyawei@koolearn.com>
2020-01-09 10:26:12 +0800

    几种this

---
+++ JavaScript/js基础/this/1.js
@@ -0,0 +1,39 @@
+    // const a = {
+    //     b: function b() {
+    //         console.log(this)
+    //     }
+    // }
+    // const c = {}
+    // c.f = a.b;
+    // c.f();
+    // a.b();
+    
+    // const d = {
+    //     b() {
+    //         console.log(this)
+    //     }
+    // }
+    // const e = {}
+    // e.f = d.b;
+    // e.f();
+    // d.b();
+
+    // const k = {
+    //     b: () => {
+    //         console.log(this)
+    //     }
+    // }
+    // const j = {}
+    // j.f = k.b;
+    // j.f();
+    // k.b();
+
+    // const h = {
+    //     b() {
+    //         console.log(this)
+    //     }
+    // }
+    // const i = {}
+    // i.f = h.b.bind(h);
+    // i.f();
+    // h.b();
