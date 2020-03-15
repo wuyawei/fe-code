de4793060a316b5b88272c9a8eb0ca964194fff2
wuyawei <wuyawei@koolearn.com>
2020-03-15 23:02:11 +0800

    执行上下文

--- JavaScript/js基础/执行原理/1.js
+++ JavaScript/js基础/执行原理/1.js
@@ -1,5 +1,19 @@
-let c = 1;
-{
-	console.log(c);
-	let c = 2;
+// function bar() {
+// 	console.log(n);
+// }
+// function foo() {
+// 	var n = 1;
+// 	bar();
+// }
+// var n = 2;
+// foo(); // 2
+
+function foo() {
+	function bar() {
+		console.log(n);
+	}
+	var n = 1;
+	bar();
 }
+var n = 2;
+foo(); // 1
