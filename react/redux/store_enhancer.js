56e02d6964176e7abcd2e7e37b58da7ab65c1733
wuyawei <wuyawei@koolearn.com>
2020-03-24 16:55:18 +0800

    middleware

--- react/redux/store_enhancer.js
+++ react/redux/store_enhancer.js
@@ -34,10 +34,24 @@ const applyMiddleware = (...middlewares) => {
         const chain = middlewares.map(middleware => middleware(middlewaresAPI));
         // compose 连接一个或多个 next => action => {}，并将 store.dispatch 传入
         // 最终得到多个中间件处理后的dispatch 类似于 f1(f2(f3(dispatch)))(action) ...
+        // 注意最终传入 action 时，middlewares[0] 是最先执行的，因为每个 middleware 返回值是函数。
+        // compose 是从右往左执行，最终得到从左到右嵌套的函数（左边第一个在最外层）。
+        // 这也是为什么 applyMiddleware 一般要放在第一个的原因，因为这里会包含异步的 action
         dispatch = compose(...chain)(store.dispatch);
         return {...store, dispatch};
     }
 }
+// c(b(a(dispatch)))(action)
+const a = n => a => n(a+1);
+const b = n => a => n(a+2);
+const c = n => a => n(a+3);
+c(b(a(console.log)))(1);
+// 等同于
+const _a = a => console.log(a+1);
+const _b = a => _a(a+2);
+const _c = a => _b(a+3);
+_c(1);
+
 // const store = createStore(reducer, applyMiddleware(logger));
 // 等同于
 // const store = applyMiddleware(logger)(createStore)(reducer, ...args);
