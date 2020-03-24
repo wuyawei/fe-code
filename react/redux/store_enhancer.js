f71cdc3558db8dcaaf89e427e8aaf6f72bbbe0b6
wuyawei <wuyawei@koolearn.com>
2020-03-24 18:36:09 +0800

    dispatch

--- react/redux/store_enhancer.js
+++ react/redux/store_enhancer.js
@@ -1,5 +1,5 @@
 // store 中间件
-// function logger({ getState }) {
+// function logger({ getState, dispatch }) {
 //     return next => action => {
 //       console.log('will dispatch', action)
 //       const returnValue = next(action)
@@ -30,6 +30,12 @@ const applyMiddleware = (...middlewares) => {
             getState: store.getState,
             dispatch: (action, ..._args) => dispatch(action, ..._args)
         }
+        // let a = () => console.log(1);
+        // const obj = { a: () => a() }; // 变量引用
+        // const obj2 = { a }; // 这种是把函数引用地址赋值给了属性 a
+        // a = () => console.log(2);
+        // obj.a();
+        // obj2.a();
         // 执行中间件，得到 next => action => {}
         const chain = middlewares.map(middleware => middleware(middlewaresAPI));
         // compose 连接一个或多个 next => action => {}，并将 store.dispatch 传入
@@ -37,20 +43,20 @@ const applyMiddleware = (...middlewares) => {
         // 注意最终传入 action 时，middlewares[0] 是最先执行的，因为每个 middleware 返回值是函数。
         // compose 是从右往左执行，最终得到从左到右嵌套的函数（左边第一个在最外层）。
         // 这也是为什么 applyMiddleware 一般要放在第一个的原因，因为这里会包含异步的 action
-        dispatch = compose(...chain)(store.dispatch);
+        dispatch = compose(...chain)(store.dispatch); // 运行后，middlewaresAPI 中的 dispatch 也会变成当前的 dispatch
         return {...store, dispatch};
     }
 }
 // c(b(a(dispatch)))(action)
-const a = n => a => n(a+1);
-const b = n => a => n(a+2);
-const c = n => a => n(a+3);
-c(b(a(console.log)))(1);
+// const a = n => a => n(a+1);
+// const b = n => a => n(a+2);
+// const c = n => a => n(a+3);
+// c(b(a(console.log)))(1);
 // 等同于
-const _a = a => console.log(a+1);
-const _b = a => _a(a+2);
-const _c = a => _b(a+3);
-_c(1);
+// const _a = a => console.log(a+1);
+// const _b = a => _a(a+2);
+// const _c = a => _b(a+3);
+// _c(1);
 
 // const store = createStore(reducer, applyMiddleware(logger));
 // 等同于
