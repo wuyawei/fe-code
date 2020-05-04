8a90d4cea86f1cd89950a06d150798297f3644d9
wuyawei <wuyawei@koolearn.com>
2020-05-04 20:24:08 +0800

    dipatchForceReactUpdate

--- react/react-redux/index.js
+++ react/react-redux/index.js
@@ -17,3 +17,8 @@
 // 一系列状态和构建等操作
 // 最终将 selectorFactory 组合好的 props 传递给 WrappedComponent
 // 之所以能拿到 store 是因为 connect 里引入了同一个 Context
+
+// dispatch 更新 store 的 state，但是 store 和 react 本身是没有关系的，dispatch 并不会直接触发 react 的更新
+// react-redux 通过 subscribeUpdates 订阅了 store 的 state 的更新，即 dispatch 会触发 subscribeUpdates
+// 而 subscribeUpdates 会关联 react 的 setState 或 useState，从而触发 react 的更新
+// 所以 react-redux 通过发布订阅把 react 和 redux 联系在一起
