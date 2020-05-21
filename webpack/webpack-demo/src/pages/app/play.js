ab7dede01e122a920abc2d7308a0b325983e852f
wuyawei <wuyawei@koolearn.com>
2020-05-21 22:49:10 +0800

    up

--- webpack/webpack-demo/src/pages/app/play.js
+++ webpack/webpack-demo/src/pages/app/play.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import 'dplayer/dist/DPlayer.min.css';
 import DPlayer from 'dplayer';
 import './style.css';
-import flv from '../../../assets/webpack/06-webpack源码【瑞客论坛 www.ruike1.com】.flv';
+import flv from '../../../assets/react/01-react-props【瑞客论坛 www.ruike1.com】.flv';
 export default function Play() {
     useEffect(() => {
         const dp = new DPlayer({
