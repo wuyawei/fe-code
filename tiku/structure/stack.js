de6e659f4cba93ebe1681b9e26369bfbda6a9532
wuyawei <wuyawei@koolearn.com>
2020-07-19 23:24:20 +0800

    stack

---
+++ tiku/structure/stack.js
@@ -0,0 +1,23 @@
+class Stack {
+    constructor() {
+        this.dataStore = [];
+    }
+    push(ele) {
+        this.dataStore.push(ele);
+    }
+    pop() {
+        return this.dataStore.pop();
+    }
+    peek() {
+        return this.dataStore[this.dataStore.length - 1]; // 获取栈顶元素
+    }
+    clear() {
+        this.dataStore = [];
+    }
+    isEmpty() {
+        return this.dataStore.length === 0;
+    }
+    get size() {
+        return this.dataStore.length;
+    }
+}
