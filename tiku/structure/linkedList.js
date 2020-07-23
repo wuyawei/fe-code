e2d46c01916f837aabf9631fff89ec3cfc2e52ff
wuyawei <wuyawei@koolearn.com>
2020-07-23 23:45:49 +0800

    insert

--- tiku/structure/linkedList.js
+++ tiku/structure/linkedList.js
@@ -1,7 +1,7 @@
 class LinkedList{
     constructor() {
-        this.length = 0;
         this.head = null;
+        this.length = 0;
     }
     node(element) {
         return {
@@ -39,6 +39,56 @@ class LinkedList{
             this.length --;
             return current.node;
         }
+        return null;
+    }
+    insert(position, element) {
+        if (position > -1 && position <= this.length){
+            const node = this.node(element);
+            let current = this.head;
+            let previous = null;
+            let index = 0;
+            if(position === 0) {
+                // 
+                node.next = current;
+                this.head = node;
+            } else {
+                while(index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = node;
+                node.next = current;
+            }
+            this.length ++;
+            return  true;
+        } else {
+            return false;
+        }
+    }
+    indexOf(element) {
+        let current = this.head;
+        let index = 0;
+        while(current) {
+            if(element === current.node) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+    remove(element) {
+        const index = this.indexOf(element);
+        return this.removeAt(index);
+    }
+    getHead() {
+        return this.head;
+    }
+    isEmpty() {
+        return this.length === 0;
+    }
+    get size() {
+        return this.length;
     }
 }
 
@@ -49,4 +99,4 @@ list.append('c');
 list.append('d');
 list.append('e');
 list.removeAt(2);
-console.log(list.head)
+console.log(list.getHead())
