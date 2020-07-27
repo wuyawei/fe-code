3fc5571e32956c5ec393bcc0add28d4fec3db6e9
wuyawei <wuyawei@koolearn.com>
2020-07-27 23:11:59 +0800

    mergeList

--- tiku/structure/linkedList.js
+++ tiku/structure/linkedList.js
@@ -89,13 +89,50 @@ class LinkedList{
     isEmpty() {
         return this.length === 0;
     }
+    toString() {
+        let current = this.head;
+        let str = '';
+        while(current.next) {
+            str += current.node;
+            current = current.next; 
+        }
+        return str;
+    }
+    replace(list) {
+        this.head = list;
+        return this;
+    }
     get size() {
         return this.length;
     }
 }
 
+// 合并链表
+const mergeList = (l1, l2) => {
+    if(l1 === null) {
+        return l2;
+    }
+    if(l2 === null) {
+        return l1;
+    }
+    if(l1.node < l2.node) {
+        l1.next = mergeList(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeList(l2.next, l1);
+        return l2;
+    }
+}
+
+const list1 = new LinkedList();
+list1.append(1);
+list1.append(3);
+list1.append(5);
+
+const list2 = new LinkedList();
+list2.append(2);
+list2.append(4);
+list2.append(5);
+
 const list = new LinkedList();
-list.append('a');
-list.append('b');
-list.insert(2, 'c')
-console.log(list.getHead())
+console.log(list.replace(mergeList(list1.getHead(), list2.getHead())).toString())
