863c3e62630982e1291a2ee3f764533da520871d
wuyawei <wuyawei@koolearn.com>
2020-07-29 23:58:38 +0800

    insertNode

--- tiku/structure/binarySearchTree.js
+++ tiku/structure/binarySearchTree.js
@@ -1,4 +1,36 @@
 // 二叉搜索树
 class BinarySearchTree {
-
+    constructor() {
+        this.root = null;
+    }
+    node(key) {
+        return {
+            key: key,
+            left: null,
+            right: null
+        }
+    }
+    insert(insertKey) {
+        const insertNode = (node, newNode) => {
+            if(newNode.key < node.key) {
+                if(node.left === null) {
+                    node.left = newNode;
+                } else {
+                    insertNode(node.left, newNode);
+                }
+            } else if(newNode.key > node.key) {
+                if(node.right === null) {
+                    node.right = newNode;
+                } else {
+                    insertNode(node.right, newNode)
+                }
+            }
+        }
+        const newNode = this.node();
+        if (this.root === null) {
+            this.root = node;
+        } else {
+            insertNode(this.root, newNode);
+        }
+    }
 }
