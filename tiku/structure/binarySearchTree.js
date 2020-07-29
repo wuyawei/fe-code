074140511c4e0da126b622b89b2bb51c8c0b6c6f
wuyawei <wuyawei@koolearn.com>
2020-07-30 00:08:02 +0800

    searchNode

--- tiku/structure/binarySearchTree.js
+++ tiku/structure/binarySearchTree.js
@@ -26,11 +26,45 @@ class BinarySearchTree {
                 }
             }
         }
-        const newNode = this.node();
+        const newNode = this.node(insertKey);
         if (this.root === null) {
             this.root = node;
         } else {
             insertNode(this.root, newNode);
         }
+    }
+    search(key) {
+        const searchNode = (node, key) => {
+            if(node === null) return false;
+            if (key < node.key) {
+                searchNode(node.left, key);
+            } else if(key > node.key) {
+                searchNode(node.right, key);
+            } else {
+                return true;
+            }
+            return false;
+        }
+        return searchNode(this.root, key);
+    }
+    max() {
+        const node = this.root;
+        while(node && node.right) {
+            node = node.right;
+        }
+        if (node) {
+            return node.key;
+        }
+        return null;
+    }
+    min() {
+        const node = this.root;
+        while(node && node.left) {
+            node = node.left;
+        }
+        if (node) {
+            return node.key;
+        }
+        return null;
     }
 }
