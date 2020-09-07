8413c50834f61f89c07a4bde309940e5758d99b1
wuyawei <wuyawei@koolearn.com>
2020-09-08 00:35:57 +0800

    createFlow

--- JavaScript/js基础/执行原理/1.js
+++ JavaScript/js基础/执行原理/1.js
@@ -8,12 +8,30 @@
 // var n = 2;
 // foo(); // 2
 
-function foo() {
-	function bar() {
-		console.log(n);
-	}
-	var n = 1;
-	bar();
+// function foo() {
+// 	function bar() {
+// 		console.log(n);
+// 	}
+// 	var n = 1;
+// 	bar();
+// }
+// var n = 2;
+// foo(); // 1
+
+const a = {
+	a: 1
 }
-var n = 2;
-foo(); // 1
+Object.defineProperty(a, 'a', {
+	// enumerable: false
+	get() {
+		return 2
+	}
+})
+// for (let k in a) {
+//     console.log("k", k);
+// }
+// console.log(Object.keys(a))
+// console.log("Object.getOwnPropertyNames(a)", Object.getOwnPropertyNames(a)) 
+// console.log("Reflect.ownKeys(a)", Reflect.ownKeys(a))
+a.a = 3;
+console.log(a.a)
---
+++ tiku/other/createFlow.js
@@ -0,0 +1,47 @@
+/* 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const subFlow = createFlow([() => delay(1000).then(() => log("c"))]);
+
+createFlow([
+  () => log("a"),
+  () => log("b"),
+  subFlow,
+  [() => delay(1000).then(() => log("d")), () => log("e")],
+]).run(() => {
+  console.log("done");
+});
+
+// 需要按照 a,b,延迟1秒,c,延迟1秒,d,e, done 的顺序打印
+*/
+var { log } = console;
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const createFlow = (arr) => {
+    const queue = arr.flat();
+    const run = async (done) => {
+        done && queue.push(done);
+        for(let i = 0; i < queue.length; i++) {
+            const fn = queue[i];
+            if (typeof fn.run === 'function') {
+                await fn.run();
+            } else {
+                await fn();
+            }
+        }
+    }
+    return {
+        run
+    };
+};
+
+
+const subFlow = createFlow([() => delay(1000).then(() => log("c"))]);
+createFlow([
+    () => log("a"),
+    () => log("b"),
+    subFlow,
+    [() => delay(1000).then(() => log("d")), () => log("e")],
+  ]).run(() => {
+    console.log("done");
+}); 
