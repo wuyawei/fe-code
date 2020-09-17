bc3a8ede76cbd84d7111e427e820e888b1a3e953
wuyawei <wuyawei@koolearn.com>
2020-09-17 23:22:22 +0800

    5

--- tiku/leetCode/5-最长子回文串.js
+++ tiku/leetCode/5-最长子回文串.js
@@ -1,3 +1,19 @@
+/* 
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+*/
+
+
 /**
  * @param {string} s
  * @return {string}
@@ -27,4 +43,25 @@ var longestPalindrome = function(s) {
     }
     return result;
 };
-longestPalindrome('adadabbcadada1')
+longestPalindrome('adadabbcadada1');
+
+/**
+ * 动态规划
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome1 = function(s) {
+    const n = s.length;
+    const dp = [];
+    let res = '';
+    for(let i = n - 1; i >= 0; i--) {
+        dp[i] = [];
+        for(let j = i; j < n; j++) {
+            dp[i][j] = s[i]==s[j] && (j - i < 2 || dp[i+1][j-1]);
+            if(dp[i][j] && j - i + 1 > res.length) {
+                res = s.slice(i, j+1);
+            }
+        }
+    }
+    return res;
+};
