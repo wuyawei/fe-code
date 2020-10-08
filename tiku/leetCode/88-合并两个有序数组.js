debb583b22a07c1c4cd72075e4363979a5d8f08c
wuyawei <wuyawei@koolearn.com>
2020-10-08 23:56:53 +0800

    88

---
+++ tiku/leetCode/88-合并两个有序数组.js
@@ -0,0 +1,40 @@
+/* 
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+ 
+
+说明:
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ 
+
+示例:
+
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+输出: [1,2,2,3,5,6]
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let len = m+n-1;
+    m--;
+    n--;
+    while(n >= 0) {
+        if(nums1[m] > nums2[n]) {
+            nums1[len--] = nums1[m--];
+        }else{
+            nums1[len--] = nums2[n--];
+        }
+    }
+};
