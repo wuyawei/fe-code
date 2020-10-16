290d5983736cc1642a60e85c63f607ec46bee944
wuyawei <wuyawei@koolearn.com>
2020-10-16 15:13:02 +0800

    239

--- tiku/leetCode/剑指 Offer 59 - I. 滑动窗口的最大值.js
+++ tiku/leetCode/剑指 Offer 59 - I. 滑动窗口的最大值.js
@@ -56,3 +56,41 @@ var maxSlidingWindow2 = function(nums, k) {
     }
     return result;
 };
+
+// 动态规划
+var maxSlidingWindow3 = function(nums, k) {
+    if(k <= 1) return nums;
+    let len = nums.length;
+    const left = [];
+    const right = [];
+    const res = [];
+    left[0] = nums[0];
+    right[len-1] = nums[len-1];
+    for (let i = 1; i < len; i++) {
+        // 如果不能取余，就交换最大值
+        // 可以取余，说明前一个窗口已经走完，直接赋值
+        // 需要注意 窗口下标是 0 1 2 而不是 1 2 3，能整除说明是新窗口的开始
+        // left 右边最大
+        if(i % k) {
+            left[i] = Math.max(nums[i], left[i -1]);
+        } else {
+            left[i] = nums[i];
+        }
+
+        // right 倒着来一次
+        let j = len - 1 - i; // 起点
+        // j + 1 是因为如果 j+1个能整除 k，说明当前这个是倒序的新窗口的第一个
+        // right 左边最大
+        if((j + 1) % k) {
+            right[j] = Math.max(nums[j], right[j+1]);
+        } else {
+            right[j] = nums[j];
+        }
+    }
+
+    // i 的截止位置是 最后一个滑动窗口的第一个位置的下标 包含这个位置
+    for(let i = 0; i <= len - k; i++) {
+        res.push(Math.max(right[i], left[i + k - 1]))
+    }
+    return res;
+}
