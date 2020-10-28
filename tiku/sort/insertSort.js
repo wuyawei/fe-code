8e1468f83cee56e9cc7bb88465295a450fd4b0a2
wuyawei <wuyawei@koolearn.com>
2020-10-28 22:46:14 +0800

    insert

--- tiku/sort/insertSort.js
+++ tiku/sort/insertSort.js
@@ -3,14 +3,21 @@ const insertSort = (arr) => {
         // 选择一个标的
         let target = i;
         //已排列表循环
+        // 一轮循环以后，说明 i（包括 i）之前的已经排序好了
         for(let j = i-1; j >= 0; j--) {
             // 插入已排列表
             if(arr[target] < arr[j]) {
                 // 小的插到前面
                 [arr[target], arr[j]] = [arr[j], arr[target]];
+                // 交换后，原来的 target 的下标，变成了j
+                // 起始下标是 i，第一次交换，是将 i-1 移动到了 i
+                // target 变为了 i - 1，即 j
+                // 所以每次 arr[target] 是同一个值依次和前面已排序的值做比较
+                // 找到最终位置
+                target = j;
+            } else {
+                break;
             }
-            // 所有的都需要往后移动一位
-            target = j;
         }
     }
     return arr;
